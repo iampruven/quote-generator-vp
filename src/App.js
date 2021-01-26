c8a178f7f85dace24669dff823df1851999d7694
Vendy Prum <vendy@thinkful.com>
2021-01-26 14:30:30 -0800

    finished functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+const {
+  randomQuote,
+  authorQuotes,
+  getQuotes,
+  searchQuotes,
+} = require("quotegarden");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    displayQuote: "",
+    author: "",
+    mainQuoteGenre:'',
+    allQuotes: [],
+    moreQuotes: false,
+  };
+  componentDidMount() {
+    fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then((quote) => {
+        this.setState({
+          displayQuote: quote.data[0].quoteText,
+          mainQuoteGenre: quote.data[0].quoteGenre,
+          author: quote.data[0].quoteAuthor,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  showAllQuoteByAuthor = (quoteAuthor) => {
+    fetch(
+      `https://quote-garden.herokuapp.com/api/v3/quotes?author=${quoteAuthor}`
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then((allQuotes) => {
+       
+        this.setState({ allQuotes: allQuotes.data, moreQuotes: true });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  
+  render() {
+
+ 
+    return (
+      <>
+        <div className="notepaper">
+          <figure className="quote">
+            <blockquote className="curly-quotes">
+              {this.state.displayQuote}
+            </blockquote>
+            <a
+              href="#"
+              onClick={() => this.showAllQuoteByAuthor(this.state.author)}
+              className="quote-by"
+            >
+              — {this.state.author}
+            </a>
+            <figcaption className="quote-by">{this.state.mainQuoteGenre}</figcaption>
+          </figure>
+        </div>
+
+        {this.state.moreQuotes &&
+          this.state.allQuotes.map((quote, id) => {
+            // console.log(quote.quoteText)
+            return <div key={id} className="notepaper">
+              <figure className="quote">
+                <blockquote className="curly-quotes">
+                  {quote.quoteText}
+                </blockquote>
+                <figcaption
+                  className="quote-by"
+                >
+                  — {this.state.author}
+                </figcaption>
+                <figcaption className="quote-by">{quote.quoteGenre}</figcaption>
+              </figure>
+            </div>
+          })}
+      </>
+    );
+  }
 }
 
 export default App;
